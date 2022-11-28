6378bd8a359a0f91a1d235752e2abae6e8e1485d
W0yzeck <115701693+W0yzeck@users.noreply.github.com>
2022-11-28 15:38:34 +0100

    Add files via upload

---
+++ Teamverwaltung - Projektdaten/Teamverwaltung - Projektdaten/Software/docker/django/gunicorn.py
@@ -0,0 +1,11 @@
+#source: https://github.com/benoitc/gunicorn/blob/master/examples/example_config.py
+import multiprocessing
+bind = '0.0.0.0:8000'
+backlog = 2048
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_connections = 1000
+timeout = 300
+graceful_timeout = 60
+keepalive = 5
+keyfile = '/var/www/ssl/htw_iteamse_server.key'
+certfile = '/var/www/ssl/htw_iteamse_server_with_ca.crt'
