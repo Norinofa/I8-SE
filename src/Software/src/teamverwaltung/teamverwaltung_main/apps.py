f1bde65646281b0338e5265749b9aa4c5e4eb6c8
W0yzeck <115701693+W0yzeck@users.noreply.github.com>
2023-01-10 15:31:26 +0100

    Add files via upload

---
+++ src/Software/src/teamverwaltung/teamverwaltung_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TeamverwaltungMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'teamverwaltung_main'
---
+++ src/Software/src/teamverwaltung/teamverwaltung_main/exceptions.py
@@ -0,0 +1,9 @@
+class CSVStudentParserException(Exception):
+    """Exception, welche von der Klasse *CSVStudentParser*
+    geworfen wird."""
+    pass
+
+class AssignmentAlgoException(Exception):
+    """Exception, welche von der Klasse *AssignmentAlgo*
+    geworfen wird."""
+    pass
