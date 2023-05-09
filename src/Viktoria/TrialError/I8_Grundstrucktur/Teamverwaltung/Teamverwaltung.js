854545fa91df8c907006cd52e9e69293969a729e
Viktoria Karklin <s83774@htw-dresden.de>
2023-05-09 09:43:21 +0200

    Docker: Prototyp Container, Vorversion

--- src/Viktoria/TrialError/I8_Grundstrucktur/Teamverwaltung/Teamverwaltung.js
+++
@@ -1,6 +0,0 @@
-function generieren() {
-    var min = parseInt(document.getElementById("min").value);
-    var max = parseInt(document.getElementById("max").value);
-    var size = min + max;
-    window.location.href = "Teamverwaltung.html?size=" + size;
-}
---
+++ src/Viktoria/VorerstFrontend/Stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop Prototype
+docker rm Prototype
