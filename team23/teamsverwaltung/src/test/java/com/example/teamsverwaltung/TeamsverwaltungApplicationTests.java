b8880f7c144648b1ec8a03214b43ae72504c9765
Anoo <115702348+nuha-adnan@users.noreply.github.com>
2023-05-23 20:06:26 +0200

    spring

---
+++ team23/teamsverwaltung/src/test/java/com/example/teamsverwaltung/TeamsverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamsverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
