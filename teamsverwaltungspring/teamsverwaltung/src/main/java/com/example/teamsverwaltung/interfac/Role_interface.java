c53196c2ec342b91af0297ce88a8a40dbb3da799
VincentHTW <115701635+VincentHTW@users.noreply.github.com>
2023-05-29 18:01:41 +0200

    Delete teamsverwaltungspring/teamsverwaltung directory

--- teamsverwaltungspring/teamsverwaltung/src/main/java/com/example/teamsverwaltung/interfac/Role_interface.java
+++
@@ -1,8 +0,0 @@
-package com.example.teamsverwaltung.interfac;
-
-import com.example.teamsverwaltung.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface Role_interface extends JpaRepository<Role,Long> {
-}
--- teamsverwaltungspring/teamsverwaltung/src/main/java/com/example/teamsverwaltung/interfac/user_interface.java
+++
@@ -1,6 +0,0 @@
-package com.example.teamsverwaltung.interfac;
-import com.example.teamsverwaltung.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface user_interface extends JpaRepository<User, Long> {
-}
