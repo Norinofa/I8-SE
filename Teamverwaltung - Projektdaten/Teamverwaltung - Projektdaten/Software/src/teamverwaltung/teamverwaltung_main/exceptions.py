6a5445b0ab157df161bfcb0c512258e68d14a228
VincentHTW <115701635+VincentHTW@users.noreply.github.com>
2023-05-29 18:00:56 +0200

    Delete Teamverwaltung - Projektdaten/Teamverwaltung - Projektdaten directory

--- Teamverwaltung - Projektdaten/Teamverwaltung - Projektdaten/Software/src/teamverwaltung/teamverwaltung_main/exceptions.py
+++
@@ -1,9 +0,0 @@
-class CSVStudentParserException(Exception):
-    """Exception, welche von der Klasse *CSVStudentParser*
-    geworfen wird."""
-    pass
-
-class AssignmentAlgoException(Exception):
-    """Exception, welche von der Klasse *AssignmentAlgo*
-    geworfen wird."""
-    pass
